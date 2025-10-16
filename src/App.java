745ad8f9b7e456265b07e2aa1cb53f3f284382bd
Ruan Victor <ruanvictor.2202@gmail.com>
2025-10-16 16:46:03 -0300

    primeiro commit

---
+++ src/App.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+public class App {
+    public static void main(String[] args) throws Exception {
+        var gerador = new Random();
+        var inteiros = new VetorDinamico(4);
+        
+        while (true){
+            var oQueFazer = gerador.nextInt(2);
+            
+            switch (oQueFazer) {
+                case 0:
+                    System.out.println("aicionando...");
+                    var elementos = gerador.nextInt(100) + 1;
+                    inteiros.adicionar(elementos);
+                    break;
+            
+                case 1:
+                    System.out.println("Removendo...");
+                    inteiros.remover();
+                    break;
+            }
+            
+            System.out.println(inteiros);
+            System.out.println("************************************");
+            Thread.sleep(2000);
+        }
+
+    }
+}
